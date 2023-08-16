c40c7d892d09a85e82d81c594ed302a383ee51c0
GuilhermeDevv <glzin3006@gmail.com>
2023-08-16 14:51:00 -0300

    feat: interface de dados do BD

--- src/config/IDatabase.ts
+++ src/config/IDatabase.ts
@@ -1,13 +1,15 @@
 import { Dialect } from "sequelize";
 
 export interface IDatabase {
-  host: string,
-  dialect: Dialect,
-  database: string,
-  username: string,
-  password: string,
-  define: {
-    timestamps: boolean,
-    underscored: boolean,
-  },
-};
+  development: DbConfig;
+  test: DbConfig;
+  production: DbConfig;
+}
+
+export interface DbConfig {
+  username: string;
+  password: string ;
+  database: string;
+  host: string;
+  dialect: Dialect 
+}
