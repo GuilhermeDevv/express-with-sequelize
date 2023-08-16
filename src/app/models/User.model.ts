08685ef398aedddd93c349157ab80a5075c02253
GuilhermeDevv <glzin3006@gmail.com>
2023-08-16 14:52:23 -0300

    feat: criação do model de users

---
+++ src/app/models/User.model.ts
@@ -0,0 +1,34 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../../database";
+
+class User extends Model {
+  public id!: number;
+  public username!: string;
+  public email!: string;
+  public password!: string;
+}
+
+User.init(
+  {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "User",
+  }
+);
+
+export default User;
